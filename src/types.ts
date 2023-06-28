8ff32b84a92339a873f559539c6f3d395203e041
Trevor Coleman <trevor@trevorcoleman.design>
2023-06-28 16:10:02 -0400

    convert comments to JSDoc comments

--- src/types.ts
+++ src/types.ts
@@ -6,29 +6,53 @@ export interface NavigationParameterSpec {
 }
 
 export interface ScreenSpec {
-  // Id is inferred when discovered in a keyed map, but needs to be explicit when in an array of screens
+  /**
+   * Id is inferred when discovered in a keyed map, but needs to be explicit when in an array of screens
+   * */
   id?: string;
-  // Name will be id (or the discovered key) if not specified
+  /**
+   * Name will be id (or the discovered key) if not specified
+   */
   name?: string;
   parameterType?: string;
-  // The base type of the parameter object
+  /**
+   * The base type of the parameter object
+   */
   extends?: string;
+  /**
+   * Parameters that are passed to the screen.
+   */
   parameters?: Array<NavigationParameterSpec>;
-  // The name of the screen for analytics events (possibly will change to allow more settings)
+  /**
+   * The name of the screen for analytics events (possibly will change to allow more settings)
+   */
   analytics?: string | false;
+  /**
+   * Specify any custom types that need to be imported for use in params etc.
+   */
   imports?: ImportSpecs[];
-  import?: ImportSpec[];
 }
 
 export interface NavigatorSpec extends ScreenSpec {
+  /**
+   * The type of the navigator.
+   */
   type: 'stack' | 'nativeStack' | 'drawer' | 'bottomTab' | 'materialBottomTab' | 'materialTopTab';
-  // The type of the navigator screen list parameters
+  /**
+   * The type of the navigator screen list parameters.
+   */
   parameterListType?: string;
-  // If each screen should receive a certain parameter type, list it here
+  /**
+   * If each screen should receive a certain parameter type, list it here
+   */
   defaultParameters?: string;
-  // Do not prefix paths of this navigator
+  /**
+   * Do not prefix paths of this navigator
+   */
   noPrefix?: boolean;
-  // The screens that are part of this navigator
+  /**
+   * The screens that are part of this navigator
+   */
   screens: { [key: string]: ScreenOrNavigator } | Array<ScreenOrNavigator>;
 }
 
